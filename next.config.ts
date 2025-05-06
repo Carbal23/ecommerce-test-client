b6b1bb6816076186f14dcfe0a49fd46b38fcedf6
Mauricio Carbal Martinez <mcarbal23@gmail.com>
2025-05-05 23:10:01 -0500

    unOptimized images true

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: [process.env.NEXT_PUBLIC_API_BASE_URL ?? "localhost", "localhost"],
+    unoptimized: true,
   },
 };
 
